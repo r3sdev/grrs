f4fb53349ee5e7959f5ed68341b9a460bb7c6df5
Ramsy de Vos <ramsy@ramsy.dev>
2024-07-30 18:07:50 +0200

    Move tests from main.rs to separate test file
    Tests previously embedded in the main.rs have been relocated to tests/test_main.rs. This follows best practices for keeping the main codebase clean and separating concerns, improving maintainability and readability.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,8 @@
 use std::fs::File;
 use std::io::{BufReader, Read};
-// Used for writing assertions
-use std::process::Command;
 
 use anyhow::{Context, Result};
-use assert_cmd::prelude::*;
-use assert_fs::prelude::*;
 use clap::Parser;
-use predicates::prelude::*;
 
 // Add methods on commands
 use grrs::find_matches;
@@ -34,35 +29,4 @@ fn main() -> Result<()> {
     find_matches(&content, &args.pattern, &mut std::io::stdout());
 
     Ok(())
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
-        let mut cmd = Command::cargo_bin("grrs")?;
-
-        cmd.arg("foobar").arg("test/file/doesnt/exist");
-        cmd.assert()
-            .failure()
-            .stderr(predicate::str::contains("could not read file"));
-
-        Ok(())
-    }
-
-    #[test]
-    fn find_content_in_file() -> Result<(), Box<dyn std::error::Error>> {
-        let file = assert_fs::NamedTempFile::new("sample.txt")?;
-        file.write_str("A test\nActual content\nMore content\nAnother test")?;
-
-        let mut cmd = Command::cargo_bin("grrs")?;
-        cmd.arg("test").arg(file.path());
-        cmd.assert()
-            .success()
-            .stdout(predicate::str::contains("A test\nAnother test"));
-
-        Ok(())
-    }
 }

5cc6aa0ac6b6c4d2939395ecaac8738be57719f1
Ramsy de Vos <ramsy@ramsy.dev>
2024-07-30 13:10:50 +0200

    Add integration tests for `grrs` using `assert_cmd` and `assert_fs`
    Integrated `assert_cmd` and `assert_fs` to add two new tests: one to check error handling when a file does not exist and another to verify correct content search in a temporary file. Updated dependencies and development dependencies in Cargo.toml accordingly.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
 use std::fs::File;
 use std::io::{BufReader, Read};
+// Used for writing assertions
+use std::process::Command;
 
 use anyhow::{Context, Result};
+use assert_cmd::prelude::*;
+use assert_fs::prelude::*;
 use clap::Parser;
+use predicates::prelude::*;
 
+// Add methods on commands
 use grrs::find_matches;
 
+// Run programs
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(Parser)]
 struct Cli {
@@ -27,4 +34,35 @@ fn main() -> Result<()> {
     find_matches(&content, &args.pattern, &mut std::io::stdout());
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
+        let mut cmd = Command::cargo_bin("grrs")?;
+
+        cmd.arg("foobar").arg("test/file/doesnt/exist");
+        cmd.assert()
+            .failure()
+            .stderr(predicate::str::contains("could not read file"));
+
+        Ok(())
+    }
+
+    #[test]
+    fn find_content_in_file() -> Result<(), Box<dyn std::error::Error>> {
+        let file = assert_fs::NamedTempFile::new("sample.txt")?;
+        file.write_str("A test\nActual content\nMore content\nAnother test")?;
+
+        let mut cmd = Command::cargo_bin("grrs")?;
+        cmd.arg("test").arg(file.path());
+        cmd.assert()
+            .success()
+            .stdout(predicate::str::contains("A test\nAnother test"));
+
+        Ok(())
+    }
 }
